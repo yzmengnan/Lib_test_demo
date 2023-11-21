67ce004915ec118f265f40f8b9129608928facc4
yangqiang <91418249@qq.com>
2023-11-21 17:14:51 +0800

    更新了joint2position的rpy角输出，设置输入为角度制，

--- inc/Transform.hpp
+++ inc/Transform.hpp
@@ -1,3 +1,5 @@
+#pragma clang diagnostic push
+#pragma ide diagnostic ignored "cppcoreguidelines-avoid-magic-numbers"
 //
 // Created by 91418 on 2023/11/6.
 //
@@ -5,83 +7,98 @@
 #ifndef JACOBDEMO_TRANSFORM_HPP
 #define JACOBDEMO_TRANSFORM_HPP
 #include "mat.hpp"
-#include "math.h"
-inline vector<float> joint2Position(const vector<float> &joint_data) {
+#include <cmath>
+inline auto angles2rad(const vector<float> &data) -> vector<float> {
+    vector<float> rads{};
+    for (const auto &d: data) {
+        rads.push_back(d / 180 * 3.1415F);
+    }
+    return rads;
+}
+
+inline auto joint2Position(const vector<float> &joint_data) -> vector<float> {
     if (joint_data.size() < 6) {
         return vector<float>{};
     }
-    mat T01{{{cos(joint_data[0]), -sin(joint_data[0]), 0, 0},
-             {sin(joint_data[0]), cos(joint_data[0]), 0, 0},
+    auto joint_rad = angles2rad(joint_data);
+    mat T01{{{cos(joint_rad[0]), -sin(joint_rad[0]), 0, 0},
+             {sin(joint_rad[0]), cos(joint_rad[0]), 0, 0},
              {0, 0, 1, 0},
              {0, 0, 0, 1}}};
-    mat T12{{{cos(joint_data[1]), -sin(joint_data[1]), 0, 0},
+    mat T12{{{cos(joint_rad[1]), -sin(joint_rad[1]), 0, 0},
              {0, 0, 1, 1.002},
-             {-sin(joint_data[1]), -cos(joint_data[1]), 0, 0},
+             {-sin(joint_rad[1]), -cos(joint_rad[1]), 0, 0},
              {0, 0, 0, 1}}};
-    mat T23{{{cos(joint_data[2]), -sin(joint_data[2]), 0, 0},
+    mat T23{{{cos(joint_rad[2]), -sin(joint_rad[2]), 0, 0},
              {0, 0, -1, 0},
-             {sin(joint_data[2]), cos(joint_data[2]), 0, 0},
+             {sin(joint_rad[2]), cos(joint_rad[2]), 0, 0},
              {0, 0, 0, 1}}};
-    mat T34{{{cos(joint_data[3]), -sin(joint_data[3]), 0, 0},
+    mat T34{{{cos(joint_rad[3]), -sin(joint_rad[3]), 0, 0},
              {0, 0, 1, 1.1115},
-             {-sin(joint_data[3]), -cos(joint_data[3]), 0, 0},
+             {-sin(joint_rad[3]), -cos(joint_rad[3]), 0, 0},
              {0, 0, 0, 1}}};
-    mat T45{{{cos(joint_data[4]), -sin(joint_data[4]), 0, 0},
+    mat T45{{{cos(joint_rad[4]), -sin(joint_rad[4]), 0, 0},
              {0, 0, -1, 0},
-             {sin(joint_data[4]), cos(joint_data[4]), 0, 0},
+             {sin(joint_rad[4]), cos(joint_rad[4]), 0, 0},
              {0, 0, 0, 1}}};
-    mat T56{{{cos(joint_data[5]), -sin(joint_data[5]), 0, 0},
+    mat T56{{{cos(joint_rad[5]), -sin(joint_rad[5]), 0, 0},
              {0, 0, 1, 0},
-             {-sin(joint_data[5]), -cos(joint_data[5]), 0, 0},
+             {-sin(joint_rad[5]), -cos(joint_rad[5]), 0, 0},
              {0, 0, 0, 1}}};
     mat T02 = T01 * T12;
     mat T03 = T02 * T23;
     mat T04 = T03 * T34;
     mat T05 = T04 * T45;
     mat T06 = T05 * T56;
-    return vector<float>{T06.data[0][3], T06.data[1][3], T06.data[2][3], 0.0f, 0.0f, 0.0f};
+    float beta = atan2(-T06.data[2][0], sqrt(T06.data[0][0] * T06.data[0][0] + T06.data[1][0] * T06.data[1][0]));
+    float alpha = atan2(T06.data[1][0] / cos(beta), T06.data[0][0] / cos(beta));
+    float gama = atan2(T06.data[2][1] / cos(beta), T06.data[2][2] / cos(beta));
+    return vector<float>{T06.data[0][3], T06.data[1][3], T06.data[2][3], alpha * 180 / 3.1415F, beta * 180 / 3.1415F, gama * 180 / 3.1415F};
 }
 
-inline mat jacob0(const vector<float> &joint_data) {
+inline auto jacob0(vector<float> &joint_data) -> mat {
     //only used for 6-DOF
     if (joint_data.size() != 6) {
-        return mat();
+        return {};
     }
-    vector<float> jacobv1{static_cast<float>(-1.002 * cos(joint_data[0]) - 1.1115 * cos(joint_data[2]) * cos(joint_data[0]) + 1.115 * sin(joint_data[0]) * cos(joint_data[1]) * sin(joint_data[2])),
-                          static_cast<float>(1.1115 * cos(joint_data[0]) * sin(joint_data[1]) * sin(joint_data[2])),
-                          static_cast<float>(1.1115 * sin(joint_data[2]) * sin(joint_data[0]) - 1.1115 * cos(joint_data[0]) * cos(joint_data[1]) * cos(joint_data[2])),
+
+    auto joint_rad = angles2rad(joint_data);
+
+    vector<float> jacobv1{-1.002F * cos(joint_rad[0]) - 1.1115F * cos(joint_rad[2]) * cos(joint_rad[0]) + 1.115F * sin(joint_rad[0]) * cos(joint_rad[1]) * sin(joint_rad[2]),
+                          1.1115F * cos(joint_rad[0]) * sin(joint_rad[1]) * sin(joint_rad[2]),
+                          1.1115F * sin(joint_rad[2]) * sin(joint_rad[0]) - 1.1115F * cos(joint_rad[0]) * cos(joint_rad[1]) * cos(joint_rad[2]),
                           0, 0, 0};
-    vector<float> jacobv2{static_cast<float>(-1.002 * sin(joint_data[0]) - 1.1115 * sin(joint_data[0]) * cos(joint_data[2]) - 1.1115 * cos(joint_data[1]) * cos(joint_data[0]) * sin(joint_data[2])),
-                          static_cast<float>(1.1115 * sin(joint_data[0]) * sin(joint_data[1]) * sin(joint_data[2])),
-                          static_cast<float>(-1.1115 * cos(joint_data[0]) * sin(joint_data[2]) - 1.1115 * sin(joint_data[0]) * cos(joint_data[1]) * cos(joint_data[2])),
+    vector<float> jacobv2{-1.002F * sin(joint_rad[0]) - 1.1115F * sin(joint_rad[0]) * cos(joint_rad[2]) - 1.1115F * cos(joint_rad[1]) * cos(joint_rad[0]) * sin(joint_rad[2]),
+                          1.1115F * sin(joint_rad[0]) * sin(joint_rad[1]) * sin(joint_rad[2]),
+                          -1.1115F * cos(joint_rad[0]) * sin(joint_rad[2]) - 1.1115F * sin(joint_rad[0]) * cos(joint_rad[1]) * cos(joint_rad[2]),
                           0, 0, 0};
     vector<float> jacobv3{0,
-                          static_cast<float>(1.1115 * cos(joint_data[1]) * sin(joint_data[2])),
-                          static_cast<float>(1.1115 * sin(joint_data[1]) * cos(joint_data[2])),
+                          1.1115F * cos(joint_rad[1]) * sin(joint_rad[2]),
+                          1.1115F * sin(joint_rad[1]) * cos(joint_rad[2]),
                           0, 0, 0};
-    mat T01{{{cos(joint_data[0]), -sin(joint_data[0]), 0, 0},
-             {sin(joint_data[0]), cos(joint_data[0]), 0, 0},
+    mat T01{{{cos(joint_rad[0]), -sin(joint_rad[0]), 0, 0},
+             {sin(joint_rad[0]), cos(joint_rad[0]), 0, 0},
              {0, 0, 1, 0},
              {0, 0, 0, 1}}};
-    mat T12{{{cos(joint_data[1]), -sin(joint_data[1]), 0, 0},
+    mat T12{{{cos(joint_rad[1]), -sin(joint_rad[1]), 0, 0},
              {0, 0, 1, 1.002},
-             {-sin(joint_data[1]), -cos(joint_data[1]), 0, 0},
+             {-sin(joint_rad[1]), -cos(joint_rad[1]), 0, 0},
              {0, 0, 0, 1}}};
-    mat T23{{{cos(joint_data[2]), -sin(joint_data[2]), 0, 0},
+    mat T23{{{cos(joint_rad[2]), -sin(joint_rad[2]), 0, 0},
              {0, 0, -1, 0},
-             {sin(joint_data[2]), cos(joint_data[2]), 0, 0},
+             {sin(joint_rad[2]), cos(joint_rad[2]), 0, 0},
              {0, 0, 0, 1}}};
-    mat T34{{{cos(joint_data[3]), -sin(joint_data[3]), 0, 0},
+    mat T34{{{cos(joint_rad[3]), -sin(joint_rad[3]), 0, 0},
              {0, 0, 1, 1.1115},
-             {-sin(joint_data[3]), -cos(joint_data[3]), 0, 0},
+             {-sin(joint_rad[3]), -cos(joint_rad[3]), 0, 0},
              {0, 0, 0, 1}}};
-    mat T45{{{cos(joint_data[4]), -sin(joint_data[4]), 0, 0},
+    mat T45{{{cos(joint_rad[4]), -sin(joint_rad[4]), 0, 0},
              {0, 0, -1, 0},
-             {sin(joint_data[4]), cos(joint_data[4]), 0, 0},
+             {sin(joint_rad[4]), cos(joint_rad[4]), 0, 0},
              {0, 0, 0, 1}}};
-    mat T56{{{cos(joint_data[5]), -sin(joint_data[5]), 0, 0},
+    mat T56{{{cos(joint_rad[5]), -sin(joint_rad[5]), 0, 0},
              {0, 0, 1, 0},
-             {-sin(joint_data[5]), -cos(joint_data[5]), 0, 0},
+             {-sin(joint_rad[5]), -cos(joint_rad[5]), 0, 0},
              {0, 0, 0, 1}}};
     mat T02 = T01 * T12;
     mat T03 = T02 * T23;
@@ -98,46 +115,48 @@ inline mat jacob0(const vector<float> &joint_data) {
     jacobw = jacobw.T();
     return mat({jacobv1, jacobv2, jacobv3, jacobw.data[0], jacobw.data[1], jacobw.data[2]});
 }
-inline mat jacobe(const vector<float> &joint_data) {
+inline auto jacobe(vector<float> &joint_data) -> mat {
     //only used for 6-DOF
     if (joint_data.size() != 6) {
-        return mat();
+        return {};
     }
-    vector<float> jacobv1{static_cast<float>(-1.002 * cos(joint_data[0]) - 1.1115 * cos(joint_data[2]) * cos(joint_data[0]) + 1.115 * sin(joint_data[0]) * cos(joint_data[1]) * sin(joint_data[2])),
-                          static_cast<float>(1.1115 * cos(joint_data[0]) * sin(joint_data[1]) * sin(joint_data[2])),
-                          static_cast<float>(1.1115 * sin(joint_data[2]) * sin(joint_data[0]) - 1.1115 * cos(joint_data[0]) * cos(joint_data[1]) * cos(joint_data[2])),
+    auto joint_rad = angles2rad(joint_data);
+    vector<float> jacobv1{-1.002F * cos(joint_rad[0]) - 1.1115F * cos(joint_rad[2]) * cos(joint_rad[0]) + 1.115F * sin(joint_rad[0]) * cos(joint_rad[1]) * sin(joint_rad[2]),
+                          1.1115F * cos(joint_rad[0]) * sin(joint_rad[1]) * sin(joint_rad[2]),
+                          1.1115F * sin(joint_rad[2]) * sin(joint_rad[0]) - 1.1115F * cos(joint_rad[0]) * cos(joint_rad[1]) * cos(joint_rad[2]),
                           0, 0, 0};
-    vector<float> jacobv2{static_cast<float>(-1.002 * sin(joint_data[0]) - 1.1115 * sin(joint_data[0]) * cos(joint_data[2]) - 1.1115 * cos(joint_data[1]) * cos(joint_data[0]) * sin(joint_data[2])),
-                          static_cast<float>(1.1115 * sin(joint_data[0]) * sin(joint_data[1]) * sin(joint_data[2])),
-                          static_cast<float>(-1.1115 * cos(joint_data[0]) * sin(joint_data[2]) - 1.1115 * sin(joint_data[0]) * cos(joint_data[1]) * cos(joint_data[2])),
+    vector<float> jacobv2{-1.002F * sin(joint_rad[0]) - 1.1115F * sin(joint_rad[0]) * cos(joint_rad[2]) - 1.1115F * cos(joint_rad[1]) * cos(joint_rad[0]) * sin(joint_rad[2]),
+                          1.1115F * sin(joint_rad[0]) * sin(joint_rad[1]) * sin(joint_rad[2]),
+                          -1.1115F * cos(joint_rad[0]) * sin(joint_rad[2]) - 1.1115F * sin(joint_rad[0]) * cos(joint_rad[1]) * cos(joint_rad[2]),
                           0, 0, 0};
     vector<float> jacobv3{0,
-                          static_cast<float>(1.1115 * cos(joint_data[1]) * sin(joint_data[2])),
-                          static_cast<float>(1.1115 * sin(joint_data[1]) * cos(joint_data[2])),
+                          1.1115F * cos(joint_rad[1]) * sin(joint_rad[2]),
+                          1.1115F * sin(joint_rad[1]) * cos(joint_rad[2]),
                           0, 0, 0};
-    mat T01{{{cos(joint_data[0]), -sin(joint_data[0]), 0, 0},
-             {sin(joint_data[0]), cos(joint_data[0]), 0, 0},
+
+    mat T01{{{cos(joint_rad[0]), -sin(joint_rad[0]), 0, 0},
+             {sin(joint_rad[0]), cos(joint_rad[0]), 0, 0},
              {0, 0, 1, 0},
              {0, 0, 0, 1}}};
-    mat T12{{{cos(joint_data[1]), -sin(joint_data[1]), 0, 0},
+    mat T12{{{cos(joint_rad[1]), -sin(joint_rad[1]), 0, 0},
              {0, 0, 1, 1.002},
-             {-sin(joint_data[1]), -cos(joint_data[1]), 0, 0},
+             {-sin(joint_rad[1]), -cos(joint_rad[1]), 0, 0},
              {0, 0, 0, 1}}};
-    mat T23{{{cos(joint_data[2]), -sin(joint_data[2]), 0, 0},
+    mat T23{{{cos(joint_rad[2]), -sin(joint_rad[2]), 0, 0},
              {0, 0, -1, 0},
-             {sin(joint_data[2]), cos(joint_data[2]), 0, 0},
+             {sin(joint_rad[2]), cos(joint_rad[2]), 0, 0},
              {0, 0, 0, 1}}};
-    mat T34{{{cos(joint_data[3]), -sin(joint_data[3]), 0, 0},
+    mat T34{{{cos(joint_rad[3]), -sin(joint_rad[3]), 0, 0},
              {0, 0, 1, 1.1115},
-             {-sin(joint_data[3]), -cos(joint_data[3]), 0, 0},
+             {-sin(joint_rad[3]), -cos(joint_rad[3]), 0, 0},
              {0, 0, 0, 1}}};
-    mat T45{{{cos(joint_data[4]), -sin(joint_data[4]), 0, 0},
+    mat T45{{{cos(joint_rad[4]), -sin(joint_rad[4]), 0, 0},
              {0, 0, -1, 0},
-             {sin(joint_data[4]), cos(joint_data[4]), 0, 0},
+             {sin(joint_rad[4]), cos(joint_rad[4]), 0, 0},
              {0, 0, 0, 1}}};
-    mat T56{{{cos(joint_data[5]), -sin(joint_data[5]), 0, 0},
+    mat T56{{{cos(joint_rad[5]), -sin(joint_rad[5]), 0, 0},
              {0, 0, 1, 0},
-             {-sin(joint_data[5]), -cos(joint_data[5]), 0, 0},
+             {-sin(joint_rad[5]), -cos(joint_rad[5]), 0, 0},
              {0, 0, 0, 1}}};
     mat T02 = T01 * T12;
     mat T03 = T02 * T23;
@@ -160,7 +179,8 @@ inline mat jacobe(const vector<float> &joint_data) {
              {0, 0, 0, T06.data[0][0], T06.data[1][0], T06.data[2][0]},
              {0, 0, 0, T06.data[0][1], T06.data[1][1], T06.data[2][1]},
              {0, 0, 0, T06.data[0][2], T06.data[1][2], T06.data[2][2]}}};
-    return Re0*jacob0;
+    return Re0 * jacob0;
 }
-
 #endif//JACOBDEMO_TRANSFORM_HPP
+
+#pragma clang diagnostic pop
