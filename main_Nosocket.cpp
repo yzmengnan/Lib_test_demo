d1e9ecc9bca59569d39fd4b16d44a4fe8ccf7394
local <914181249@shu.edu.cn>
2023-12-12 13:40:13 +0800

    更新

--- main_Nosocket.cpp
+++ main_Nosocket.cpp
@@ -1,8 +1,10 @@
 #include "DATA_STRUCT.h"
 #include "Data_Log.hpp"
 #include "Multi_Process.h"
+#include "Transform.hpp"
 #include "driverSever.h"
 #define BasicTest
+#define operationalControlTest
 using namespace std;
 int main() {
     Tc_Ads ads_ptr;
@@ -14,10 +16,8 @@ int main() {
     d->Enable();
 #ifdef BasicTest
     d->setSyncrpm(100);
-    d->Write('1', 0.0f, 0.0f, 0.0f);
-    Sleep(5000);
-    d->Write('1', 0.0f, 0.0f, 0.0f);
-    Sleep(30000);
+    d->Write('2', 10.0F, 10.0F,10.0F, 10.0F, 10.0F,10.0F);
+    Sleep(10000);
     cout << "Finished!" << endl;
 #else
     vector<DTS> cspSendData(servoNums, DTS{});
@@ -25,6 +25,28 @@ int main() {
     while (true) {
         d->servoCSP(cspSendData, cspGetData);
     }
+#endif
+#ifdef operationalControlTest
+    auto c_begin = mat(fkine(MDT::getAngles(*d, d->MotGetData)));
+    c_begin.print("operational position: ");
+//    vector<float> c_target{-0.551,2.032,0.034,32.135F,-29.162F,-84.174F};
+    vector<float> c_vecs{-1,0,0,0,0,0};
+
+    d->setSyncrpm(50);
+    for(int i{};i<100*6;i++){
+        d->opSpaceMotionByJacob(c_vecs);
+        Sleep(5);
+    }
+    vec  c_end = mat(fkine(MDT::getAngles(*d,d->MotGetData)));
+    c_end.print("operational position end:");
+    vec c_delta = {-0.2,0,0,0,0,0};
+    vec c_newTarget = {-0.529,2.036,0.097,39.0059,-47.2695,-73.2263};
+    c_newTarget.print("c_New target:");
+    d->setSyncrpm(100);
+    d->opSpaceMotion(vector<double>{c_newTarget.begin(),c_newTarget.end()});
+    Sleep(8000);
+    vec c_newEnd = fkine(MDT::getAngles(*d,d->MotGetData));
+    c_newEnd.print("c_new end: ");
 #endif
     p.processDelete(pi);
     //    system("pause");
