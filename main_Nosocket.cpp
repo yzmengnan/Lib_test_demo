10caee43f4724cb9c9d6c29d98927edf0ab37047
yangqiang <91418249@qq.com>
2023-12-29 16:32:07 +0800

    update

--- main_Nosocket.cpp
+++ main_Nosocket.cpp
@@ -3,8 +3,8 @@
 #include "Multi_Process.h"
 #include "Transform.hpp"
 #include "driverSever.h"
-#define BasicTest
-//#define operationalControlTest
+//#define BasicTest
+#define operationalControlTest
 //#define VISUAL
 using namespace std;
 auto getVisualSocket() -> shared_ptr<SOCKET> {
@@ -83,45 +83,102 @@ int main() {
     d->Enable();
 #ifdef BasicTest
     d->setSyncrpm(100);
-    d->Write('x', 20.0F, 0.0F, -40.0F, 0.0F, 20.0F, 0.0F);
+        d->Write('x', 20.0F, 0.0F, -40.0F, 0.0F, 20.0F, 0.0F);
     cout << "Finished!" << endl;
 #endif
 #ifdef operationalControlTest
-    auto c_begin = mat(fkine(MDT::getAngles(*d, d->MotGetData)));
-    c_begin.print("operational position: ");
-    //    vector<float> c_target{-0.551,2.032,0.034,32.135F,-29.162F,-84.174F};
-    vector<float> c_vecs{0, 0, 0, 0, 0, 0};
-    d->setSyncrpm(100);
+    d->setSyncrpm(10);
+    vector<float> c_vecs{-1, 0, 0, 0, 0, 0};
     for (int i{}; i < 100 * 6; i++) {
-        d->opSpaceMotionByJacob(c_vecs);
+        d->opSpaceMotionByJacobe(c_vecs);
         Sleep(5);
+//        vec c_end = mat(fkine_W(MDT::getAngles(*d, d->MotGetData)));
+//        c_end.t().print("operational position end:");
     }
-    return 0;
-    vec c_end = mat(fkine(MDT::getAngles(*d, d->MotGetData)));
-    c_end.print("operational position end:");
-    vec c_delta = {0, 0, 0.5, 0, 0, 0};
-    vec c_newTarget = c_end + c_delta;
-    c_newTarget.print("c_New target:");
-    d->setSyncrpm(50);
-    d->opSpaceMotion(vector<double>{c_newTarget.begin(), c_newTarget.end()}, 100);
-    //    Sleep(20000);
-    vec c_newEnd = fkine(MDT::getAngles(*d, d->MotGetData));
-    c_newEnd.print("c_new end: ");
+//    d->setSyncrpm(10);
+#ifdef jacobeMotionTest
+    {
+        cout << "move down!" << endl;
+        for (int i{}; i < 100 * 6; i++) {
+            d->opSpaceMotionByJacob(c_vecs);
+            Sleep(5);
+            vec c_end = mat(fkine_W(MDT::getAngles(*d, d->MotGetData)));
+            c_end.t().print("operational position end:");
+        }
+        Sleep(1000);
+        cout << "move up!" << endl;
+        c_vecs = {0, -1, 0, 0, 0, 0};
+        for (int i{}; i < 100 * 6; i++) {
+            d->opSpaceMotionByJacob(c_vecs);
+            Sleep(5);
+            vec c_end = mat(fkine_W(MDT::getAngles(*d, d->MotGetData)));
+            c_end.t().print("operational position end:");
+        }
+        Sleep(1000);
+        cout << "move right!" << endl;
+        c_vecs = {1, 0, 0, 0, 0, 0};
+        for (int i{}; i < 100 * 6; i++) {
+            d->opSpaceMotionByJacob(c_vecs);
+            Sleep(5);
+            vec c_end = mat(fkine_W(MDT::getAngles(*d, d->MotGetData)));
+            c_end.t().print("operational position end:");
+        }
+        Sleep(1000);
+        cout << "move left!" << endl;
+        c_vecs = {-1, 0, 0, 0, 0, 0};
+        for (int i{}; i < 100 * 6; i++) {
+            d->opSpaceMotionByJacob(c_vecs);
+            Sleep(5);
+            vec c_end = mat(fkine_W(MDT::getAngles(*d, d->MotGetData)));
+            c_end.t().print("operational position end:");
+        }
+        Sleep(1000);
+        cout << "rotate with x" << endl;
+        c_vecs = {0, 0, 0, 1, 0, 0};
+        for (int i{}; i < 100 * 6; i++) {
+            d->opSpaceMotionByJacob(c_vecs);
+            Sleep(5);
+            vec c_end = mat(fkine_W(MDT::getAngles(*d, d->MotGetData)));
+            c_end.t().print("operational position end:");
+        }
+        Sleep(1000);
+        cout << "rotate with -x" << endl;
+        c_vecs = {0, 0, 0, -1, 0, 0};
+        for (int i{}; i < 100 * 6; i++) {
+            d->opSpaceMotionByJacob(c_vecs);
+            Sleep(5);
+            vec c_end = mat(fkine_W(MDT::getAngles(*d, d->MotGetData)));
+            c_end.t().print("operational position end:");
+        }
+        return 0;
+    }
+#endif
+//    vec c_end = mat(fkine_W(MDT::getAngles(*d, d->MotGetData)));
+//    c_end.print("operational position end:");
+//    vec c_delta = {-0.1, 0, 0, 0, 0, 0};
+//    vec c_newTarget = c_end + c_delta;
+//    c_newTarget.print("c_New target:");
+//    d->setSyncrpm(10);
+//        d->opSpaceMotion(vector<double>{c_newTarget.begin(), c_newTarget.end()});
+//        Sleep(20000);
+//    vec c_newEnd = fkine(MDT::getAngles(*d, d->MotGetData));
+//    c_newEnd.print("c_new end: ");
 #endif
 #ifdef VISUAL
+    d->setSyncrpm(1);
     auto socketptr = getVisualSocket();
     if (*socketptr != INVALID_SOCKET) {
         char recvbuf[56] = {};
         while (recv(*socketptr, recvbuf, 56, 0)) {
             auto c_vecs = (double *) &recvbuf;
             cout << c_vecs[0] << "," << c_vecs[1] << "," << c_vecs[2] << "," << c_vecs[3] << "," << c_vecs[4] << "," << c_vecs[5] << endl;
-            d->opSpaceMotionByJacob(vector<float>{(float) c_vecs[0], (float) c_vecs[1], (float) c_vecs[2], 0, 0, 0});
+            d->opSpaceMotionByJacobe(vector<float>{(float) c_vecs[1], -(float) c_vecs[0], (float) c_vecs[2], (float) c_vecs[3], (float) c_vecs[4], (float) c_vecs[5]});
             Sleep(5);
         }
     }
 
 #endif
-    p.processDelete(pi);
-    //    system("pause");
+        p.processDelete(pi);
+//        system("pause");
     return 0;
 }
