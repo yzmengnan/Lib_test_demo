129863ecd68c50c1b743f58ddaead04351daee09
Yang <914181249@qq.com>
2024-01-23 16:30:00 +0800

    修正插补Nan问题

--- src/pathInterpolation.cpp
+++ src/pathInterpolation.cpp
@@ -20,12 +20,13 @@ auto _interpolation::_1_Order_Linear_no_time_adjustment(const vector<vector<doub
     }
     return res;
 }
-vector<vector<double>> _interpolation::_3_Order_CubicSpline_is_time_adjustment(vector<vector<double>> yData, size_t totalTime) {
+vector<vector<double>> _interpolation::_3_Order_CubicSpline_is_time_adjustment(vector<vector<double>> yData, const int& totalTime) {
     Eigen::MatrixXd yDataMat(yData.size(), yData[0].size());
     for (int i{}; i < yData.size(); i++) {
         yDataMat.row(i) = Eigen::VectorXd::Map(yData[i].data(), yData[i].size());
     }
-    auto trajectoryPointsCount{10 * totalTime};
+//    cout<<yDataMat<<endl;
+    auto trajectoryPointsCount{100 * totalTime};
     size_t pathPointsCount{yData.size()};
     size_t jointsCount{yData[0].size()};
     Eigen::VectorXd deltaBuffer(pathPointsCount - 1);
@@ -34,21 +35,25 @@ vector<vector<double>> _interpolation::_3_Order_CubicSpline_is_time_adjustment(v
             Eigen::VectorXd buffer = Eigen::Map<Eigen::VectorXd, Eigen::Unaligned>(yData[i].data(), (long long) jointsCount);
             Eigen::VectorXd bufferNext = Eigen::Map<Eigen::VectorXd, Eigen::Unaligned>(yData[i + 1].data(), (long long) jointsCount);
             Eigen::VectorXd delta = (bufferNext - buffer);
+//            cout<<delta<<endl;
             deltaBuffer[i] = delta.norm();
         }
     }
+//    cout<<"deltaBuffer"<<deltaBuffer<<endl;
     //normalize
     deltaBuffer.normalize();
     vector<double> timeArray;
     timeArray.push_back(0);
+//    cout<<"derltaBuffer"<<endl<<deltaBuffer<<endl;
     double base = deltaBuffer.lpNorm<1>();
     for (int i{}; i < deltaBuffer.size(); i++) {
         if (i >= 1)
-            timeArray.push_back((int) (deltaBuffer[i] / base * trajectoryPointsCount + (int) timeArray[i]));
+            timeArray.push_back((int) (deltaBuffer[i] / base * trajectoryPointsCount + (int) timeArray[i])+1);
         else
             timeArray.push_back((int) (deltaBuffer[i] / base * trajectoryPointsCount));
     }
     yDataMat.transposeInPlace();
+    cout<<yDataMat<<endl;
     vector<_1D::CubicSplineInterpolator<double>> it(yDataMat.rows());
     for (int i{}; i < jointsCount; i++) {
         Eigen::VectorXd x = yDataMat.row(i);
