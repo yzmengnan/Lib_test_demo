d54959e06d83520bc41badbfb9e49b810e3c06e0
local <914181249@shu.edu.cn>
2024-03-12 17:20:47 +0800

    添加Grap

--- inc/Driver.h
+++ inc/Driver.h
@@ -32,7 +32,7 @@ public:
                                            -181.22 * ANGLES_2_PULSES,
                                            -144.9 * 5 / 3 * ANGLES_2_PULSES,
                                            -33.0 * 5 / 3 * ANGLES_2_PULSES,
-                                           -25*1000 * 1.5 * ANGLES_2_PULSES};
+                                           -25 * 1000 * 1.5 * ANGLES_2_PULSES};
     //    vector<double> _driver_gearRatioScalar{
     //            -284.9231 * ANGLES_2_PULSES,
     //            -213.7 * ANGLES_2_PULSES,
@@ -95,11 +95,10 @@ public:
      * @param cst
      */
     void servoOperationModeSet(int pp, int csp, int cst) {
-        //TODO: 修改逻辑
+        // 修改逻辑
         //        pp_Flag = pp;
         //        csp_Flag = csp;
         //        cst_Flag = cst;
-        // TODO: 测试验证模式变化
         if (pp_Flag && csp) {
             pp_Flag = false;
             vector<DTS> a(servoNums);
@@ -110,7 +109,7 @@ public:
             }
             p_ads->set(a);
             this->servoEnable(a, b);
-            // TODO: csp_Flag 不能通过其他方式改变
+            //  csp_Flag 不能通过其他方式改变
             //            csp_Flag = true;
         } else if (pp_Flag && cst) {
             pp_Flag = false;
@@ -136,7 +135,6 @@ public:
             this->servoEnable(a, b);
             pp_Flag = true;
         }
-
     }
 
     virtual ~Driver();
@@ -331,7 +329,7 @@ public:
     int opSpaceMotion(const vector<double> &target_c_position);
     //    int opSpaceMotion(const vector<double> &target_c_position, int rate);
     int opSpaceMotionByJacobe(const vector<float> &c_vecs);
-    int opSpaceMotionByJacobe_RL(const vector<float> &c_vecs,Robot& robot);
+    int opSpaceMotionByJacobe_RL(const vector<float> &c_vecs, Robot &robot);
     int opSpaceMotionByJacob0(const vector<float> &c_vecs);
     int opSpaceMotionByJacob0_RL(const vector<float> &c_vecs);
 
@@ -390,11 +388,11 @@ public:
         Delta.normalize();
         //find the smallest element
         double smallone{1.0f};
-        for(const auto& item:Delta) {
-            if(abs(item)>=0.001)
-            smallone = item<=smallone? item:smallone;
+        for (const auto &item: Delta) {
+            if (abs(item) >= 0.001)
+                smallone = item <= smallone ? item : smallone;
         }
-        double k =1.0F/smallone;
+        double k = 1.0F / smallone;
         for (int vec_index = 0; vec_index < servoNums; vec_index++) {
             MotSendData[vec_index].Profile_Velocity = sync_rpm * Delta[vec_index] * k * 8388608 / 60;
             MotSendData[vec_index].Max_Velocity = 3000;
@@ -432,4 +430,134 @@ private:
     vector<DTS> MotSendData{vector<DTS>(servoNums)};
     vector<DTS> &gearRatioScalar(initializer_list<double> args);
     double sync_rpm{DEFAULT_SYNC_RPM};
+};
+
+class Grap_Driver {
+public:
+    //    Grap_Driver(TcAds_Grap_Position_Control &adsHandle) : TcAds_ptr(&adsHandle) {}
+    //T1 send T2 get
+    template<typename T1, typename T2, typename T3>
+    int Enable(T1 &send, T2 &get, const T3 &TcAds_ptr) {
+        for (int enable_try_count{}; enable_try_count < 3; enable_try_count++) {
+            uint8_t state{};
+            int16_t error_code{};
+            error_code = TcAds_ptr->get(get);
+            if (error_code < 0)
+                cout << "GRAP SERVO ENABLE: GET DATA ERROR: " << error_code << endl;
+            Sleep(10);
+            //第一次检查，检验是否已经上过使能了
+            for (const auto child: get)
+                state += (child.Status_Word & 0x37) == 0x37;
+            if (state == Grap_Position_Servo_Nums) {
+                cout << "All servos has been Enabled!" << endl;
+                //需要更新当前控制字的引用
+                for (auto &child: send)
+                    child.Control_Word |= 0xf;
+                state = 0;
+                this->enable_flag = true;
+                return 0;
+            }
+            //简化版本，根据当前零差伺服控制器版本，只校验状态字BIT12是否为0
+            //(BIT12为1状态时，可能需要重新上下电
+            for (const auto child: get)
+                state += (child.Status_Word & 0x1000) >> 12;
+            if (state != 0) {
+                cout << "Servo Enable Invalid! Please re-power the servo!" << endl;
+                return -1;
+            }
+            for (auto &child: send)
+                child.Control_Word = 0x26;
+            error_code = TcAds_ptr->set(send);
+            Sleep(40);
+            for (auto &child: send)
+                child.Control_Word = 0x27;
+            error_code = TcAds_ptr->set(send);
+            Sleep(40);
+            //Servo Enable!
+            for (auto &child: send)
+                child.Control_Word = 0x2f;
+            error_code = TcAds_ptr->set(send);
+            if (error_code < 0)
+                cout << "SERVO ENABLE FAILURE: " << error_code << endl;
+            else {
+                Sleep(100);
+                error_code = TcAds_ptr->get(get);
+                if (error_code < 0)
+                    cout << "GRAP SERVO ENABLE: GET DATA ERROR: " << error_code << endl;
+                Sleep(10);
+                for (const auto child: get)
+                    state += (child.Status_Word & 0x37) == 0x37;
+                if (state == Grap_Position_Servo_Nums) {
+                    cout << "All servos  Enabled Success!" << endl;
+                    this->enable_flag = true;
+                    return 0;
+                }
+            }
+            cout << "Grap Servo Enable Try counts: " << enable_try_count << endl;
+        }
+        cout << "Grap Servo Enable failure!" << endl;
+        return -2;
+    }
+    template<typename T1, typename T2, typename T3>
+    int Disable(T1 &send, T2 &get, const T3 &TcAds_ptr) {
+        for (auto &child: send)
+            child.Control_Word = 0x20;
+        auto error_code = TcAds_ptr->set(send);
+        if (error_code < 0) {
+            cout << "SERVO DISABLE: Get Data Error:" << error_code << '\n';
+            return -1;
+        } else
+            cout << "SERVO DISABLE SUCCESS!" << endl;
+        this->enable_flag = false;
+        return 0;
+    }
+    virtual int Enable() = 0;
+    virtual int Disable() = 0;
+    virtual int Motion(initializer_list<int32_t> target_list) = 0;
+    bool enable_flag = false;
+    virtual ~Grap_Driver() { cout << "Grap Driver QUIT!" << endl; };
+};
+
+class Grap_Driver_Position : public Grap_Driver {
+public:
+    Grap_Driver_Position(TcAds_Grap_Position_Control &adsHandle) {
+        this->driver_p_ads = &adsHandle;
+    }
+    ~Grap_Driver_Position() {
+        if (this->enable_flag)
+            this->d_Disable();
+    }
+    auto d_Disable() -> int {
+        return Disable();
+    }
+    virtual auto Enable() -> int {
+        return Grap_Driver::Enable(this->SendData_P, this->GetData_P, this->driver_p_ads);
+    }
+    virtual auto Disable() -> int {
+        return Grap_Driver::Disable(this->SendData_P, this->GetData_P, this->driver_p_ads);
+    }
+    virtual int Motion(initializer_list<int32_t> target_list);
+
+private:
+    vector<DFG_P> GetData_P{vector<DFG_P>(Grap_Position_Servo_Nums)};
+    vector<DTG_P> SendData_P{vector<DTG_P>(Grap_Position_Servo_Nums)};
+    TcAds_Grap_Position_Control *driver_p_ads = nullptr;
+};
+class Grap_Driver_Torque : public Grap_Driver {
+public:
+    Grap_Driver_Torque(TcAds_Grap_Torque_Control&adsHandle){
+            this->driver_t_ads = &adsHandle;
+    };
+    virtual auto Enable() -> int final {
+        return Grap_Driver::Enable(this->SendData_T, this->GetData_T, this->driver_t_ads);
+    }
+    virtual auto Disable()->int final{
+       return Grap_Driver::Disable(this->SendData_T,this->GetData_T,this->driver_t_ads);
+    }
+    virtual int Moition(initializer_list<int32_t> target_list) final;
+
+private:
+    vector<DFG_T> GetData_T{vector<DFG_T>(Grap_Position_Servo_Nums)};
+    vector<DTG_T> SendData_T{vector<DTG_T>(Grap_Position_Servo_Nums)};
+    TcAds_Grap_Torque_Control *driver_t_ads = nullptr;
 };
