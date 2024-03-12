16209e41349a5c03fa27364afce827abb519b20a
local <914181249@shu.edu.cn>
2024-03-12 17:21:09 +0800

    添加Grap专用Ads接口

--- inc/Tc_Ads.h
+++ inc/Tc_Ads.h
@@ -13,20 +13,49 @@
 #include "thread"
 #include <vector>
 //
-#include "windows.h"
 #include "TcAdsDef.h"
+#include "windows.h"
 //
 #include "TcAdsAPI.h"
 
+#define PLC2_PORT 852
+#define PLC1_PORT 851
 using namespace std;
 class Tc_Ads {
-public: Tc_Ads();
-    auto set(vector<DTS> &SendData)->int;
-    auto get(vector<DFS> &GetData)->int;
+public:
+    Tc_Ads();
+    auto set(vector<DTS> &SendData) -> int;
+    auto get(vector<DFS> &GetData) -> int;
     PAmsAddr pAddr = &Addr;
+
 private:
-    long nPort{},nErr{};
+    long nPort{}, nErr{};
     AmsAddr Addr{};
 };
-using pTc_Ads = Tc_Ads*;
+using pTc_Ads = Tc_Ads *;
+
+// 创建两个类，Grap为手爪Ethercat 控制部分，需要独立的Ethercat地址与操作业务逻辑
+class TcAds_Grap_Position_Control {
+#define Grap_Position_Servo_Nums 2
+public:
+    TcAds_Grap_Position_Control();
+    int set(vector<DTG_P> &SendData);
+    int get(vector<DFG_P> &GetData);
+    PAmsAddr pAddr = &Addr;
+
+private:
+    AmsAddr Addr{};
+    long nPort{}, nErr{};
+};
+class TcAds_Grap_Torque_Control {
+#define Grap_Torque_Servo_Nums 1
+public:
+    TcAds_Grap_Torque_Control();
+    int set(vector<DTG_T> &SendData);
+    int get(vector<DFG_T> &GetData);
+    PAmsAddr pAddr = &Addr;
+private:
+    long nPort{}, nErr{};
+    AmsAddr Addr{};
+};
 #endif//SEAL_DEMO_TC_ADS_H
