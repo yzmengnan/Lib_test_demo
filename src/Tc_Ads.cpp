9ba54dcb762416c4f793b5e1c0404f9e9ab67361
local <914181249@shu.edu.cn>
2024-03-12 17:19:42 +0800

    更新 Grap专用的ads接口

--- src/Tc_Ads.cpp
+++ src/Tc_Ads.cpp
@@ -12,8 +12,8 @@ Tc_Ads::Tc_Ads() {
     nErr = AdsGetLocalAddress(pAddr);
     if (nErr != 0)
         cout << "Error: Ads: Open port: " << nErr << endl;
-    pAddr->port = 851;
-    cout<<"ADS CONNECTION BUILT!"<<endl;
+    pAddr->port = PLC1_PORT;
+    cout << "ADS CONNECTION BUILT!" << endl;
 }
 /*
  * &description: send data
@@ -30,9 +30,62 @@ auto Tc_Ads::set(vector<DTS> &SendData) -> int {
  * @description: get data
  */
 auto Tc_Ads::get(vector<DFS> &GetData) -> int {
-    nErr = AdsSyncReadReq(pAddr,INPUT_BASE,INPUT_OFFSET,DFS_SIZE*GetData.size(),GetData.data());
-    if(nErr){
-//        cout<<"Error: Ads get error: "<<nErr<<endl;
+    nErr = AdsSyncReadReq(pAddr, INPUT_BASE, INPUT_OFFSET, DFS_SIZE * GetData.size(), GetData.data());
+    if (nErr) {
+        //        cout<<"Error: Ads get error: "<<nErr<<endl;
+        return -102;
+    }
+    return 0;
+}
+
+TcAds_Grap_Position_Control::TcAds_Grap_Position_Control() {
+    nPort = AdsPortOpen();
+    nErr = AdsGetLocalAddress(pAddr);
+    if (nErr != 0)
+        cout << "Error: Ads: Open port: " << nErr << endl;
+    pAddr->port = PLC2_PORT;
+    cout << "GRAP ADS CONNECTION BUILT!" << endl;
+}
+int TcAds_Grap_Position_Control::set(vector<DTG_P> &SendData) {
+
+    nErr = AdsSyncWriteReq(pAddr, OUTPUT_BASE, OUTPUT_OFFSET, DTG_SIZE_P * SendData.size(), SendData.data());
+    if (nErr) {
+        cout << "Error: Ads send error: " << nErr << endl;
+        return -101;
+    }
+    return 0;
+}
+int TcAds_Grap_Position_Control::get(vector<DFG_P> &GetData) {
+
+    nErr = AdsSyncReadReq(pAddr, INPUT_BASE, INPUT_OFFSET, DFG_SIZE_P * GetData.size(), GetData.data());
+    if (nErr) {
+        //        cout<<"Error: Ads get error: "<<nErr<<endl;
+        return -102;
+    }
+    return 0;
+}
+TcAds_Grap_Torque_Control::TcAds_Grap_Torque_Control() {
+    nPort = AdsPortOpen();
+    nErr = AdsGetLocalAddress(pAddr);
+    if (nErr != 0)
+        cout << "Error: Ads: Open port: " << nErr << endl;
+    pAddr->port = PLC2_PORT;
+    cout << "GRAP ADS CONNECTION BUILT!" << endl;
+}
+int TcAds_Grap_Torque_Control::set(vector<DTG_T> &SendData) {
+
+    nErr = AdsSyncWriteReq(pAddr, OUTPUT_BASE, OUTPUT_OFFSET_GRAP_TORQUE, DTG_SIZE_T * SendData.size(), SendData.data());
+    if (nErr) {
+        cout << "Error: Ads send error: " << nErr << endl;
+        return -101;
+    }
+    return 0;
+}
+int TcAds_Grap_Torque_Control::get(vector<DFG_T> &GetData) {
+
+    nErr = AdsSyncReadReq(pAddr, INPUT_BASE, INPUT_OFFSET_GRAP_TORQUE, DFG_SIZE_T * GetData.size(), GetData.data());
+    if (nErr) {
+        //        cout<<"Error: Ads get error: "<<nErr<<endl;
         return -102;
     }
     return 0;
