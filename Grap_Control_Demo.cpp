22ab74ace57653693fb27f1cba3728bb4d80cbb1
yangqiang <91418249@qq.com>
2024-03-13 15:03:50 +0800

    更新测试内容

--- Grap_Control_Demo.cpp
+++ Grap_Control_Demo.cpp
@@ -5,10 +5,20 @@
 
 int main() {
     TcAds_Grap_Position_Control gp_ads{};
+    TcAds_Grap_Torque_Control gt_ads{};
     Grap_Driver_Position gp(gp_ads);
-    Grap_Driver *g=&gp;
+    Grap_Driver_Torque gt(gt_ads);
+//    Grap_Driver *g=&gp;
+    Grap_Driver *g=&gt;
     g->Enable();
-    g->Motion({10});
+    g->Motion({100});
+    Sleep(2000);
+    g = &gp;
+    g->Enable();
+    g->Motion({1000,2000});
     Sleep(5000);
+    g->Disable();
+    g = &gt;
+    g->Disable();
 //    gp.Disable();
 }
