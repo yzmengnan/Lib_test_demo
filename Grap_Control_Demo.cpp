d397d2dff9166e53e24a561d039062394b04e02f
yzmengnan <914181249@qq.com>
2024-06-09 13:02:40 +0800

    手爪更新

--- Grap_Control_Demo.cpp
+++ Grap_Control_Demo.cpp
@@ -4,34 +4,34 @@
 #include "grap_action.h"
 int main(int argc, char** argv)
 {
-	//	EndEffector_Motion em;
-	//	em.Enable();
-	//	if (argc >= 2)
-	//	{
-	//		int signal = atoi(argv[1]);
-	//		switch (signal)
-	//		{
-	//			case 0:
-	//				cout << "Close!" << endl;
-	//				em.grap_tool(GRAP_CLOSE);
-	//				break;
-	//			case 1:
-	//				cout << "Open!" << endl;
-	//				em.grap_tool(GRAP_OPEN);
-	//				break;
-	//		}
-	//	}
+	EndEffector_Motion em;
+	em.Enable();
+	if (argc >= 2)
+	{
+		int signal = atoi(argv[1]);
+		switch (signal)
+		{
+			case 0:
+				cout << "Close!" << endl;
+				em.grap_tool(GRAB_CLOSE);
+				break;
+			case 1:
+				cout << "Open!" << endl;
+				em.grap_tool(GRAB_OPEN);
+				break;
+		}
+	}
 
-	EndEffector_Position ep1;
-	EndEffector_Torque et;
-	et.Enable();
-	et.Motion({0, 0, 500, 500});
+	//	EndEffector_Position ep1;
+		EndEffector_Torque et;
+		et.Enable();
+		et.Motion({0, 0, -1000, -1000});
 
-	Sleep(2000);
-	//	em.ftmr(ROTATE_FORWARD);
+	//	Sleep(2000);
+//		em.ftmr(ROTATE_BACKWARD);
 	//	Sleep(2000);
 	////	em.ftmr(ROTATE_BACKWARD);
-	//	em.grap_tool(GRAP_CLOSE);
+//	em.grap_tool(GRAP_CLOSE);
 	system("pause");
 	return 0;
 }
